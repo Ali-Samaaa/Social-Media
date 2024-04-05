807689fbd67c4bd46a027b55b8fd22b20ac497cc
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-04-05 22:23:28 +0330

    create authenticate

--- A/settings.py
+++ A/settings.py
@@ -124,4 +124,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATE_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authenticate.EmailBackend",
+]
+
 LOGIN_URL = '/account/login/'
